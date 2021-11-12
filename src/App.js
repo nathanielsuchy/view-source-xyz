2ac4c47e6b3226e6adbf7b3ec86f7e8f4e574475
Catgirl <8227641+irlcatgirl@users.noreply.github.com>
2021-11-12 23:04:09 +0000

    Iniital commit

--- src/App.js
+++ src/App.js
@@ -1,8 +1,62 @@
 import React from 'react';
+const Hljs = require("highlight.js");
+window.hljs = Hljs;
+require("highlightjs-line-numbers.js");
+Hljs.highlightAll();
+        Hljs.initLineNumbersOnLoad({
+          singleLine: true
+        });
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {value: 'https://www.example.com/', code: ''};
 
-function App() {
-  const value = 'World';
-  return <div>Hello {value}</div>;
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleChange(event) {
+    this.setState({value: event.target.value});
+  }
+
+  handleSubmit(event) {
+    fetch(this.state.value)
+    .then((resp) => resp.text())
+    .then((text) => {
+      this.setState({code: text})
+    })
+    .then(() => {
+        Hljs.highlightAll();
+        Hljs.initLineNumbersOnLoad({
+          singleLine: true
+        });
+    })
+    .catch((e) => {
+      console.log(e)
+    })
+    event.preventDefault();
+  }
+
+  render() {
+    return(<div className="container">
+    <h1>View Source Kids Edition</h1>
+    <p>Google Chrome now allows ADMINISTATORS to block view-source with Enterprise Policies and that's mean 😭 so I fixed the bug 😼
+      <br /><br />
+      Remember kids, ask your parents for permission before going online!
+      <br /><br />
+      This website requires you to install <a href="https://chrome.google.com/webstore/detail/cross-domain-cors/mjhpgnbimicffchbodmgfnemoghjakai" style={{
+        color: 'white',
+      }}>Chrome Extension</a> to bypass CORS Restrictions. We are working on a better bypass in the near future.
+      <br /><br />
+      <strong>Enter website URL:</strong><br />
+      <form onSubmit={this.handleSubmit}>
+      <input value={this.state.value} onChange={this.handleChange} className='urlInput' type='url' />
+      <input type='submit' className='urlSubmit' value='Can I please see the source? 🥺' />
+      </form>
+      </p>
+      <pre className='source'><code id="sourceCode">{this.state.code}</code></pre>
+  </div>)
+  }
 }
 
 export default App;
